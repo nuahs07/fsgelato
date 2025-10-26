74a82adcb62023cd83bbf6a7697ce9dc1fedaaab
SOMa <shaunmallari09@gmail.com>
2025-10-27 00:52:40 +0800

    Initialize Angular project with core structure
    Set up a new Angular application including configuration files, core modules, components, services, models, assets, and environment files. This initial commit establishes the project structure, adds recommended VS Code settings, and includes a README with development instructions.

---
+++ product/src/src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../service/menu.service';
+import { Menu } from '../model/menu';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit  {
+  public menus: Menu[] = []
+
+  constructor(private menuService: MenuService) {
+  }
+
+  ngOnInit(): void {
+      this.menuService.getData().subscribe(data => {this.menus = data; });
+  }
+}
