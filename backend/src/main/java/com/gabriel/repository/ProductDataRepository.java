cac1bea92e5e675e648a3a274b32094d52d4cd18
SOMa <shaunmallari09@gmail.com>
2025-11-05 01:40:02 +0800

    final

--- backend/src/main/java/com/gabriel/repository/ProductDataRepository.java
+++ backend/src/main/java/com/gabriel/repository/ProductDataRepository.java
@@ -1,8 +1,7 @@
 package com.gabriel.repository;
 
 import com.gabriel.entity.ProductData;
-import com.gabriel.model.ProductCategory;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductDataRepository extends CrudRepository<ProductCategory, Integer> {
+public interface ProductDataRepository extends CrudRepository<ProductData, Integer> {
 }
